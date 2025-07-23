56470b7f4fd7c6bb384c32a66301a191c81d0cfe
Omar Aldawud <omaldawud@noctrl.edu>
2025-07-23 16:29:55 -0500

    🔥 Removed all test files and configs/comm/sdfvidsf

--- dashboard/dashboard-client/src/components/functional/index.js
+++
@@ -1,5 +0,0 @@
-// src/components/functional/index.js
-//
-export { default as Overview } from "./Overview";
-export { default as Collections } from "./Collections";
-export { default as Exports } from "./Exports";
--- db/connect-to-shipping-app.js
+++ db/connect-to-shipping-app.js
@@ -1,14 +1,21 @@
+// db/connect-to-shipping-app.js
 import dotenv from "dotenv";
 dotenv.config();
+
 import { MongoClient } from "mongodb";
 
 const uri = process.env.MONGO_URI;
 const dbName = "shippingDB";
+
+if (!uri) {
+  throw new Error("❌ MONGO_URI is not defined");
+}
+
 const client = new MongoClient(uri);
 
 export const connectDB = async () => {
   try {
-    if (!client.isConnected?.()) await client.connect();
+    await client.connect();
     return client.db(dbName);
   } catch (err) {
     console.error("❌ Failed to connect to DB:", err);
